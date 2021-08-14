c11a85521f14466f6418ae8716b888c64d473b7b
Sai Surya Putra Reddy Sabbella <sabbellaputrareddy@gmail.com>
2021-08-14 20:33:32 +0530

    New Commit

--- main.py
+++ main.py
@@ -5,28 +5,29 @@ from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.popup import Popup
 from datetime import date
+from dateutil import relativedelta
 
 class MainApp(App):
     def build(self):
         self.icon = 'icon.png'
-        layout = BoxLayout(orientation='vertical', padding = (0,0,0,760), spacing = 1)
+        layout = BoxLayout(orientation='vertical', padding = (0,0,0,900), spacing = 1)
         self.heading = Label(text='Compound Interest Calculator', size_hint=(1,.2))
         layout.add_widget(self.heading)
         self.principalheading = Label(text='Enter Principal', size_hint=(1,.2))
         layout.add_widget(self.principalheading)
-        self.principal = TextInput(multiline=False, size_hint= (1, .17), input_type = 'number', input_filter='float')
+        self.principal = TextInput(multiline=False, size_hint= (1, .1), input_type = 'number', input_filter='float')
         layout.add_widget(self.principal)
         self.startheading = Label(text='Enter Start Date in DD/MM/YYYY', size_hint=(1, .2))
         layout.add_widget(self.startheading)
-        self.start = TextInput(multiline=False, size_hint= (1, .17), input_type = 'number')
+        self.start = TextInput(multiline=False, size_hint= (1, .1), input_type = 'number')
         layout.add_widget(self.start)
         self.endheading = Label(text='Enter End Date in DD/MM/YYYY', size_hint=(1, .2))
         layout.add_widget(self.endheading)
-        self.end = TextInput(multiline=False, size_hint= (1, .17), input_type = 'number')
+        self.end = TextInput(multiline=False, size_hint= (1, .1), input_type = 'number')
         layout.add_widget(self.end)
         self.rateheading = Label(text='Enter Rate of Interest in Rupees \n  (Divide it by 12 if it is in %)', size_hint=(1, .2))
         layout.add_widget(self.rateheading)
-        self.rate = TextInput(multiline=False, size_hint= (1, .17), input_filter = 'float', input_type = 'number')
+        self.rate = TextInput(multiline=False, size_hint= (1, .1), input_filter = 'float', input_type = 'number')
         layout.add_widget(self.rate)
         self.btn = Button(text = 'Calculate', size_hint= (1, .17))
         self.btn.bind(on_press=self.show)
@@ -36,8 +37,8 @@ class MainApp(App):
     def show(self,btn):
         layout = BoxLayout(orientation='vertical')
         try:
-            time,Amount = self.program(self.principal,self.start,self.end,self.rate)
-            popupLabel = Label(text=str(time)+" Days \nAmount : "+"  "+str(Amount))
+            duration,interest,Amount = self.program(self.principal,self.start,self.end,self.rate)
+            popupLabel = Label(text=str(duration.years)+" Years "+str(duration.months)+" Months "+str(duration.days)+" Days \n""Interest : "+"  "+str(interest)+"\nAmount : "+"  "+str(Amount))
             layout.add_widget(popupLabel)
         except:
             label = Label(text = 'Enter Valid Input')
@@ -56,17 +57,20 @@ class MainApp(App):
         d1 = date(int(t1[6:10]), int(t1[3:5]), int(t1[0:2]))
         d2 = date(int(t2[6:10]), int(t2[3:5]), int(t2[0:2]))
         number = d2 - d1
+        dur = relativedelta.relativedelta(d2, d1)
         p = int(p)
         t = number.days
-        ti = t
         r = float(r)
+        i1 = 0
         while (t > 365):
             i = p * r * 365 / 3000
+            i1 = i1 + i
             p = p + i
             t = t - 365
         i = p * r * t / 3000
+        i1 = i1 + i
         A = p + i
-        return ti,A
+        return dur,i1,A
 
 
 MainApp().run()
--- start.py
+++ start.py
@@ -1,20 +1,29 @@
 from datetime import date
+from dateutil import relativedelta
 
 p = input("Enter Amount :")
 t1 = input("Enter Starting Date in Format DD/MM/YYYY :")
-t2 = input("Enter Final Date :")
+t2 = input("Enter Final Date in Format DD/MM/YYYY :")
 #09-03-2002
 d1 = date(int(t1[6:10]), int(t1[3:5]), int(t1[0:2]))
 d2 = date(int(t2[6:10]), int(t2[3:5]), int(t2[0:2]))
 number = d2-d1
+dur = relativedelta.relativedelta(d2, d1)
 r = input("Enter Rate of Interest :")
 p = int(p)
 t = number.days
 r = float(r)
+i1 = 0
 while(t>365):
     i = p*r*365/3000
+    i1 = i1+i
     p = p + i
     t = t - 365
 i = p*r*t/3000
+i1 = i1 + i
+print('{} Years'.format(dur.years))
+print('{} Months'.format(dur.months))
+print('{} Days'.format(dur.days))
+print(i1)
 A = p + i
 print(A)
